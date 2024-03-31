03248faba66a590b08188c8548bfaf505a9a7b7c
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-04-01 00:34:02 +0100

    Spoon that was a stupid idea
    YOU FUCKING IDIOT

--- db/DatabaseHandler.py
+++ db/DatabaseHandler.py
@@ -11,36 +11,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 class DatabaseHandler:
-    _instance = None
-    _initialised = False
-
-    """
-    Creating a makeshift singleton object via an instance check
-    
-    Regardless of how many times DatabaseHandler is called, only
-    one instance will be used. This allows for direct access
-    anywhere in the program without tying it to a variable, making
-    it more secure and minimizing the risk of accidental overwriting
-    """
-
-    def __new__(cls, *args, **kwargs):
-        if not cls._instance:
-            logger.info("Creating a new instance of the Database Handler")
-            cls._instance = super().__new__(cls, *args, **kwargs)
-        return cls._instance
-
-    """
-    Ensures that the DatabaseHandler is initialised
-    only once, allowing for direct access anywhere
-    """
     def __init__(self):
-        if not self._initialised:
-            logger.info("INITIALISING")
-            self._initialised = True
+        pass
 
+
+
+database_handler = DatabaseHandler()
 
 if __name__ == "__main__":
     logger.info("AGFSDAG")
-    x = DatabaseHandler()
-    y = DatabaseHandler()
-    z = DatabaseHandler()
