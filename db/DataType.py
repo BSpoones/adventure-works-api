44f9ca5a2bbe4d529e99122b600ef73dd0094029
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-04-09 23:44:08 +0100

    feat: DataType

---
+++ db/DataType.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+import MySQLdb, logging
+from MySQLdb.cursors import Cursor
+logger = logging.getLogger(__name__)
+
+
+class DataType(Enum):
+    RECORD = 0,
+    RECORDS = 1,
+    COLUMN = 2,
+    COUNT = 3
+
+    def get_data(self, cur: Cursor):
+        match self:
+            case self.RECORD:
+                return cur.fetchone()
+            case self.RECORDS:
+                return cur.fetchall()
+            case self.COLUMN:
+                return [item[0] for item in cur.fetchall()]
+            case self.COUNT:
+                return int(cur.fetchone()[0])
+            case _:
+                raise KeyError("Invalid data type")
